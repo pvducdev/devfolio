c1cb2e3588e3f585fd30ed499745ea06020c7767
pvducc <pvducc.dev@gmail.com>
2025-11-04 22:50:12 +0700

    feat: add breadcrumb navigator

--- src/components/layout/status-footer.tsx
+++ src/components/layout/status-footer.tsx
@@ -1,15 +1,34 @@
 import ThemeSwitcher from "@/components/theme-switcher.tsx";
+import {
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  BreadcrumbList,
+  BreadcrumbPage,
+  BreadcrumbSeparator,
+} from "@/components/ui/breadcrumb";
 import { Gitlab } from "@/components/ui/svgs/gitlab";
 import { Linkedin } from "@/components/ui/svgs/linkedin";
 import { SITE_CONFIG } from "@/config/site.ts";
 import { Badge } from "../ui/badge";
-
 export default function StatusFooter() {
   return (
     <footer className="flex h-6 w-full items-center justify-between bg-sidebar px-3">
-      <small className="text-muted-foreground text-xs leading-none">
-        Home &gt; Welcome
-      </small>
+      <Breadcrumb>
+        <BreadcrumbList className="gap-1">
+          <BreadcrumbItem>
+            <BreadcrumbLink className="text-muted-foreground text-xs" href="/">
+              Home
+            </BreadcrumbLink>
+          </BreadcrumbItem>
+          <BreadcrumbSeparator />
+          <BreadcrumbItem>
+            <BreadcrumbPage className="text-muted-foreground text-xs">
+              Welcome
+            </BreadcrumbPage>
+          </BreadcrumbItem>
+        </BreadcrumbList>
+      </Breadcrumb>
       <div className="flex items-center space-x-4">
         {SITE_CONFIG.features.showThemeSwitcher && <ThemeSwitcher />}
         <div className="flex items-center space-x-2">
